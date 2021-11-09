4ff6ae55f38e695b01fb065893b9aa60f09992e2
nicoletimson21 <izzy7476@gmail.com>
2021-11-08 19:27:34 -0500

    Simon buttons working when clicked

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-console.log ('hello')
+const computer = []
+const player = []
+
+const red = 'red'
+const blue = 'blue'
+const green = 'green'
+const yellow = 'yellow'
+
+const redBtn = document.querySelector('.red')
+const blueBtn = document.querySelector('.blue')
+const greenBtn = document.querySelector('.green')
+const yellowBtn = document.querySelector('.yellow')
+
+redBtn.addEventListener('click', e => {
+    if (e.target.classList.contains('red'))
+        redBtn.style.opacity = "90%"
+        setTimeout(function(){
+            redBtn.style.opacity = "40%"
+        },300)
+          
+})
+
+blueBtn.addEventListener('click', e => {
+    if (e.target.classList.contains('blue'))
+        blueBtn.style.opacity = "90%"
+        setTimeout(function(){
+            blueBtn.style.opacity = "40%"
+        },300)
+          
+})
+greenBtn.addEventListener('click', e => {
+    if (e.target.classList.contains('green'))
+        greenBtn.style.opacity = "90%"
+        setTimeout(function(){
+            greenBtn.style.opacity = "40%"
+        },300)
+       
+})
+
+yellowBtn.addEventListener('click', e => {
+    if (e.target.classList.contains('yellow'))
+        yellowBtn.style.opacity = "90%"
+        setTimeout(function(){
+            yellowBtn.style.opacity = "40%"
+        },300)
+       
+    
+})
