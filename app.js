e4232294d9bed1fc8dd5089677a65687d3e87f20
nicoletimson21 <izzy7476@gmail.com>
2021-11-09 21:06:13 -0500

    Adding random number to computer array

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-const computer = []
-const player = []
+const computerChoices = []
+const playerChoices = []
+let currentPlayer
 let game = false
 
-
-const red = 'red'
-const blue = 'blue'
-const green = 'green'
-const yellow = 'yellow'
 
 const redBtn = document.querySelector('.red')
 const blueBtn = document.querySelector('.blue')
@@ -15,40 +11,40 @@ const yellowBtn = document.querySelector('.yellow')
 const startBtn = document.querySelector('.start')
 const quitBtn = document.querySelector('.quit')
 
-redBtn.addEventListener('click', e => {
+let red = redBtn.addEventListener('click', e => {
         redBtn.style.opacity = "90%"
         setTimeout(function(){
             redBtn.style.opacity = "40%"
         },300)        
 })
 
-blueBtn.addEventListener('click', e => {
+let blue = blueBtn.addEventListener('click', e => {
         blueBtn.style.opacity = "90%"
         setTimeout(function(){
             blueBtn.style.opacity = "40%"
         },300)         
 })
-greenBtn.addEventListener('click', e => {
+let green = greenBtn.addEventListener('click', e => {
         greenBtn.style.opacity = "90%"
         setTimeout(function(){
             greenBtn.style.opacity = "40%"
         },300)     
 })
 
-yellowBtn.addEventListener('click', e => {
+let yellow = yellowBtn.addEventListener('click', e => {
         yellowBtn.style.opacity = "90%"
         setTimeout(function(){
             yellowBtn.style.opacity = "40%"
         },300)       
 })
 
-let randomColor = function getRandomIntInclusive(min, max){
+let randomColor = (min, max) => {
     min = Math.ceil(min)
     max = Math.floor(max)
     console.log(Math.floor(Math.random() * (max - min +1)+ min))
     return Math.floor(Math.random() * (max - min +1)+ min)
 }
-
+// let randomValue = randomColor()
 function getRandomColor() {
     let randomValue = randomColor(1,4)
 
@@ -58,6 +54,7 @@ function getRandomColor() {
                 redBtn.style.opacity = "40%"
             },300)
             console.log(randomColor)
+            return randomValue
         } 
 
         else if (randomValue === 2){
@@ -66,6 +63,7 @@ function getRandomColor() {
                 blueBtn.style.opacity = "40%"
             },300)
             console.log(randomColor)
+            return randomValue
         } 
 
         else if (randomValue === 3){
@@ -74,6 +72,7 @@ function getRandomColor() {
                 greenBtn.style.opacity = "40%"
             },300)
             console.log(randomColor)
+            return randomValue
         } 
 
         else if (randomValue === 4){
@@ -82,17 +81,28 @@ function getRandomColor() {
                 yellowBtn.style.opacity = "40%"
             },300)
             console.log(randomColor)
+            return randomValue
         } 
         else {
             console.log('Something is not working')
         }
+        return randomValue
 }
-
+let r
 startBtn.addEventListener('click', e => {
-    if (game === false){
-        getRandomColor()
-    }
+    // if (game === false){
+            r = getRandomColor()
+            computerChoices.push(r)
+            console.log(computerChoices) 
 })
+
+
+// })
+
+// for (let i=0; i < computer.length; i++){
+
+// }
+
 
 // getRandomColor()
 
