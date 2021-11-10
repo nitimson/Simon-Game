cbde526d2d7903cb729171b734fbfb4bee14c96c
nicoletimson21 <izzy7476@gmail.com>
2021-11-09 19:18:55 -0500

    Page loads without color flashing GOOD

--- app.js
+++ app.js
@@ -13,47 +13,42 @@ const blueBtn = document.querySelector('.blue')
 const greenBtn = document.querySelector('.green')
 const yellowBtn = document.querySelector('.yellow')
 const startBtn = document.querySelector('.start')
+const quitBtn = document.querySelector('.quit')
 
 redBtn.addEventListener('click', e => {
-    if (e.target.classList.contains('red'))
         redBtn.style.opacity = "90%"
         setTimeout(function(){
             redBtn.style.opacity = "40%"
-        },300)
-          
+        },300)        
 })
 
 blueBtn.addEventListener('click', e => {
-    if (e.target.classList.contains('blue'))
         blueBtn.style.opacity = "90%"
         setTimeout(function(){
             blueBtn.style.opacity = "40%"
-        },300)
-          
+        },300)         
 })
 greenBtn.addEventListener('click', e => {
-    if (e.target.classList.contains('green'))
         greenBtn.style.opacity = "90%"
         setTimeout(function(){
             greenBtn.style.opacity = "40%"
-        },300)
-       
+        },300)     
 })
 
 yellowBtn.addEventListener('click', e => {
-    if (e.target.classList.contains('yellow'))
         yellowBtn.style.opacity = "90%"
         setTimeout(function(){
             yellowBtn.style.opacity = "40%"
-        },300)
-        
+        },300)       
 })
+
 let randomColor = function getRandomIntInclusive(min, max){
     min = Math.ceil(min)
     max = Math.floor(max)
     console.log(Math.floor(Math.random() * (max - min +1)+ min))
     return Math.floor(Math.random() * (max - min +1)+ min)
 }
+
 function getRandomColor() {
     let randomValue = randomColor(1,4)
 
@@ -91,20 +86,15 @@ function getRandomColor() {
         else {
             console.log('Something is not working')
         }
-
 }
 
 startBtn.addEventListener('click', e => {
-    if (e.target.classList.contains('start'))
-       getRandomColor()
-          
+    if (game === false){
+        getRandomColor()
+    }
 })
-// if 
-// while (game === false){
-    
-// }
 
-getRandomColor()
+// getRandomColor()
 
 // let test = Math.floor(Math.random() * 4)+1
 // console.log(test)
