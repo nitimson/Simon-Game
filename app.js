5d13c68be2b4240b8f57e483daed92800ac1e1be
nicoletimson21 <izzy7476@gmail.com>
2021-11-10 20:39:28 -0500

    compareChoices function added

--- app.js
+++ app.js
@@ -1,58 +1,111 @@
-const computerChoices = []
-const playerChoices = []
-let currentPlayer
+let computerChoices = []
+let playerChoices = []
+let level
+
 let game = false
 
 const redBtn = document.querySelector('.red')
 const blueBtn = document.querySelector('.blue')
 const greenBtn = document.querySelector('.green')
 const yellowBtn = document.querySelector('.yellow')
-const startBtn = document.querySelector('.start')
+const startGame = document.querySelector('.start')
 const quitBtn = document.querySelector('.quit')
 
+// ------- This compares computer array with player array -----------
+function compareChoices(){
+   for (i=0; i < computerChoices.length; i ++){
+       if (computerChoices[i] != playerChoices[i]){
+       return false
+       }
+   }
+   return true
+}
+
+// --------- Functions to make color opacity change ----------
+function red(){
+    redBtn.style.opacity = "90%"
+        setTimeout(function(){
+            redBtn.style.opacity = "40%"
+        },300)
+}
+
+function blue(){
+    blueBtn.style.opacity = "90%"
+        setTimeout(function(){
+            blueBtn.style.opacity = "40%"
+        },300)
+}
+function green(){
+    greenBtn.style.opacity = "90%"
+        setTimeout(function(){
+            greenBtn.style.opacity = "40%"
+        },300)
+}
+function yellow(){
+    yellowBtn.style.opacity = "90%"
+        setTimeout(function(){
+            yellowBtn.style.opacity = "40%"
+        },300)
+}
+
+// ------------ Click events for each color, changes opacity, and pushes # into player array, then compares computerChoices array to playerChoices array.  ---------------------------------------------------------
+
 redBtn.addEventListener('click', e => {
-        const redColor = 1
+        let redColor = 1
         redBtn.style.opacity = "90%"
         setTimeout(function(){
             redBtn.style.opacity = "40%"
         },300)
         playerChoices.push(redColor)
         console.log(playerChoices)
+        // function red(){
+        //     redBtn.style.opacity = "90%"
+        //         setTimeout(function(){
+        //             redBtn.style.opacity = "40%"
+        //         },300)
+        // }
+        compareChoices()
+        console.log(compareChoices)
         return playerChoices
+
 })
 
 blueBtn.addEventListener('click', e => {
-        const blueColor = 2
+        let blueColor = 2
         blueBtn.style.opacity = "90%"
         setTimeout(function(){
             blueBtn.style.opacity = "40%"
         },300)      
         playerChoices.push(blueColor)
         console.log(playerChoices)
+        compareChoices()
         return playerChoices  
 })
 
 greenBtn.addEventListener('click', e => {
-        const greenColor = 3
+        let greenColor = 3
         greenBtn.style.opacity = "90%"
         setTimeout(function(){
             greenBtn.style.opacity = "40%"
         },300) 
         playerChoices.push(greenColor)
         console.log(playerChoices)
+        compareChoices()
         return playerChoices    
 })
 
 yellowBtn.addEventListener('click', e => {
-        const yellowColor = 4
+        let yellowColor = 4
         yellowBtn.style.opacity = "90%"
         setTimeout(function(){
             yellowBtn.style.opacity = "40%"
         },300)   
         playerChoices.push(yellowColor)
         console.log(playerChoices)
+        compareChoices()
         return playerChoices    
 })
+
 
 let randomColor = (min, max) => {
     min = Math.ceil(min)
@@ -60,7 +113,8 @@ let randomColor = (min, max) => {
     console.log(Math.floor(Math.random() * (max - min +1)+ min))
     return Math.floor(Math.random() * (max - min +1)+ min)
 }
-// let randomValue = randomColor()
+
+// -------- This generates a random # from 1-4 and changes the opacity of that button (color). ------------
 function getRandomColor() {
     let randomValue = randomColor(1,4)
 
@@ -104,25 +158,69 @@ function getRandomColor() {
         }
         return randomValue
 }
+
+// ------- Click 'Start' button will cause computer to choose random color and change it's opacity, then push that value into the computerChoices array. ---------------------------------------
+
 let r
-startBtn.addEventListener('click', e => {
-    // if (game === false){
+
+startGame.addEventListener('click', e => {
             r = getRandomColor()
             computerChoices.push(r)
+            // displayComputerChoices()
             console.log(computerChoices) 
 })
 
+// -------  
+function displayComputerChoices(){
+    for (i=0; i < computerChoices.length; i++){
+        if (computerChoices[i] === 1){
+            red()
+            // redBtn.style.opacity = "90%"
+            // setTimeout(function(){
+            //     redBtn.style.opacity = "40%"
+            // },300)
+            // console.log(randomColor)
+           
+        } 
+
+        else if (computerChoices[i] === 2){
+            blue()
+            // blueBtn.style.opacity = "90%"
+            // setTimeout(function(){
+            //     blueBtn.style.opacity = "40%"
+            // },300)
+            // console.log(randomColor)
+            
+        } 
+
+        else if (computerChoices[i] === 3){
+            green()
+            // greenBtn.style.opacity = "90%"
+            // setTimeout(function(){
+            //     greenBtn.style.opacity = "40%"
+            // },300)
+            // console.log(randomColor)
+        }
+
+        else if (computerChoices[i] === 4){
+            yellow()
+            // yellowBtn.style.opacity = "90%"
+            // setTimeout(function(){
+            //     yellowBtn.style.opacity = "40%"
+            // },300)
+            // console.log(randomColor)
+            
+        } 
+        else {
+            console.log('Something is not working')
+        }
+        
+    }
+}
 
 
-// })
-
-// for (let i=0; i < computer.length; i++){
-
-// }
-
-
-// getRandomColor()
-
-// let test = Math.floor(Math.random() * 4)+1
-// console.log(test)
-// randomColor(1,4)
+function nextTurn(){
+    r = getRandomColor()
+    computerChoices.push(r)
+    console.log(computerChoices) 
+}
